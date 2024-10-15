492e18f97b9a657ff55872f1f7bcedffcb001e19
Ribson Roy <ribsonr@gmail.com>
2024-10-15 12:43:01 +0530

    Updated config

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    "react-native-webview": {
+      platforms: {
+        ios: null, // if you don’t want to override ios autolinking
+        android: null, // if you don’t want to override android autolinking
+      },
+    },
+  },
+};
